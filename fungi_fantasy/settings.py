749c7e764b269b62a2e61609243fad3def8f1f9b
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-04-04 11:06:29 +0100

    Initial attempt at deployment

--- fungi_fantasy/settings.py
+++ fungi_fantasy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-66joptaak&cq&!*c591s0wa@$j=(8a#^slvu2x&t^@swegcg47
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fungi-fantasy-4819ad1964c1.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +123,18 @@ WSGI_APPLICATION = 'fungi_fantasy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
