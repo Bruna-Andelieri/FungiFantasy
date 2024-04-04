459323c1539d60d91a7c79f15d41cfbe7e9f741a
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-04-04 11:26:22 +0100

    Deploy: remove secret key

--- fungi_fantasy/settings.py
+++ fungi_fantasy/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-66joptaak&cq&!*c591s0wa@$j=(8a#^slvu2x&t^@swegcg47'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fungi-fantasy-4819ad1964c1.herokuapp.com', 'localhost']
 
