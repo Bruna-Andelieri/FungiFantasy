d530259c8e45dc321299311ae0151945ad3e8dc9
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-04-04 20:46:56 +0100

    Feat: Add cache control

--- fungi_fantasy/settings.py
+++ fungi_fantasy/settings.py
@@ -182,6 +182,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fungi-fantasy'
     AWS_S3_REGION_NAME = 'eu-north-1'
