d68f8b8dbf943472f5d5253c2a67a0bca1d3fef2
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-04-09 23:22:50 +0100

    Feat: Add page 404

--- fungi_fantasy/urls.py
+++ fungi_fantasy/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -29,3 +30,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'fungi_fantasy.views.handler404'
