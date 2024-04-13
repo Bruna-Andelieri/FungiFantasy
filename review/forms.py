ebfd22c21d9e942c21495523a41eddd868b1d87a
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-04-13 18:45:56 +0100

    Style: add style to edit product

--- review/forms.py
+++ review/forms.py
@@ -5,3 +5,9 @@ class ReviewForm(forms.Form):
     title = forms.CharField(max_length=200)
     rating = forms.IntegerField(min_value=1, max_value=5)
     text = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['placeholder'] = f"{field_name} here ..."
