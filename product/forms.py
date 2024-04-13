662122e1312961893d351c3ec996115618d72bfd
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-04-13 18:29:13 +0100

    Style: add product edit style

--- product/forms.py
+++ product/forms.py
@@ -15,4 +15,4 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['placeholder'] = field_name
